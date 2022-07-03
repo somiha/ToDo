ad93d995622dabe49c3e222632e45158e1a6db09
Somiha <stasnimjes@gmail.com>
2022-07-04 00:16:19 +0600

    first commit

---
+++ api/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+from datetime import datetime, timedelta
+
+# Create your models here.
+
+def get_deadline():
+    return datetime.today() + timedelta(days=1)
+
+class Task(models.Model):
+  title = models.CharField(max_length=200)
+  completed = models.BooleanField(default=False, blank=True, null=True)
+  start = models.DateTimeField(default=timezone.now)
+  end = models.DateTimeField(default=get_deadline)
+
+      
+  def __str__(self):
+    return self.title
